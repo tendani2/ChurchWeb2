eafd91eda11469658d63a4613f00ce352cb15f09
tendani2 <trmunyai@gmail.com>
2017-06-17 21:52:20 +0200

    Add files via upload
    First commit

---
+++ assets/lib/calendar.script.js
@@ -0,0 +1,87 @@
+
+	$(document).ready(function() {
+		
+		$('#calendar').fullCalendar({
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek,agendaDay'
+			},
+			defaultDate: new Date(),
+			navLinks: true, // can click day/week names to navigate views
+			selectable: false,
+			selectHelper: true,
+			select: function(start, end) {
+				var title = prompt('Event Title:');
+				var eventData;
+				if (title) {
+					eventData = {
+						title: title,
+						start: start,
+						end: end
+					};
+					$('#calendar').fullCalendar('renderEvent', eventData, false); // stick? = true
+				}
+				$('#calendar').fullCalendar('unselect');
+			},
+			editable: false,
+			eventLimit: false, // allow "more" link when too many events
+			events: [
+				{
+					title: 'All Day Event',
+					start: '2017-06-16'
+				},
+				{
+					title: 'Long Event',
+					start: '2017-06-16',
+					end: '2017-06-17'
+				},
+				{
+					id: 999,
+					title: 'Repeating Event',
+					start: '2017-04-09T16:00:00'
+				},
+				{
+					id: 999,
+					title: 'Repeating Event',
+					start: '2017-04-16T16:00:00'
+				},
+				{
+					title: 'Conference',
+					start: '2017-04-11',
+					end: '2017-04-13'
+				},
+				{
+					title: 'Meeting',
+					start: '2017-04-12T10:30:00',
+					end: '2017-04-12T12:30:00'
+				},
+				{
+					title: 'Lunch',
+					start: '2017-04-12T12:00:00'
+				},
+				{
+					title: 'Meeting',
+					start: '2017-04-12T14:30:00'
+				},
+				{
+					title: 'Happy Hour',
+					start: '2017-04-12T17:30:00'
+				},
+				{
+					title: 'Dinner',
+					start: '2017-04-12T20:00:00'
+				},
+				{
+					title: 'Birthday Party',
+					start: '2017-04-13T07:00:00'
+				},
+				{
+					title: 'Click for Google',
+					url: 'http://index#form1-d',
+					start: '2017-04-28'
+				}
+			]
+		});
+		
+	});
